4614cdd72bdf0dd50927bebc15d0876770a9cf2d
YusufYuso <yusuf.elsaed@gmail.com>
2023-07-13 13:58:52 -0700

    Adding File: quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - A c program that prints a line to the standard error
+ * main - A c program that prints to the standard error
  *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
 return (1);
 }
