03b51ccd13e69c82f697ae50310c26a777f66c27
YusufYuso <yusuf.elsaed@gmail.com>
2023-07-18 16:09:19 -0700

    Adding File: 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,8 @@ void print_to_98(int n)
 
 		for (i = n; i >= 98; i--)
 		{
-			_putchar(i + '0');
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 			_putchar(',');
 			_putchar(' ');
 		}
@@ -24,7 +25,8 @@ void print_to_98(int n)
 
 		for (i = n; i < 98; i++)
 		{
-			_putchar(i + '0');
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 			_putchar(',');
 			_putchar(' ');
 		}
