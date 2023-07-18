9c9b973c432fcc6fea4eb154308028eb80ad065a
YusufYuso <yusuf.elsaed@gmail.com>
2023-07-18 12:24:19 -0700

    Adding File: 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - prints 10 times the alphabet, in lowercase ends with \n
+*/
+
+void print_alphabet_x10(void)
+{
+	for (int i = 0; i <= 10; i++)
+		print_alphabet();
+}
