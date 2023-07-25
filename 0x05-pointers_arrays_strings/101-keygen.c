0d5abc5a0369b20815044a0c20171db0fedbcdfc
YusufYuso <yusuf.elsaed@gmail.com>
2023-07-25 16:04:44 -0700

    Adding File: 101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Generates random valid passwords for the
+ *        program 101-crackme.
+ * getPassword - function to make a random password
+ * 
+ * Return: Always 0.
+ */
+void getPassword(int len) 
+{
+	char possibleChars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()";
+	char password[len+1];
+	
+	srand(time(0)); // seed for random number generation
+	for(int i = 0; i < len; i++) 
+	{
+		int randomIndex = rand() % (sizeof(possibleChars) - 1);
+		password[i] = possibleChars[randomIndex];
+	}
+	
+	password[len] = '\0'; // null terminate the string
+	printf("%s\n", password);
+}
+
+int main() 
+{
+	int len = 10; // desired length of password
+	getPassword(len);
+	return 0;
+}
+
