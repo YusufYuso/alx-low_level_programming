5727f19f93acb8cbcdb5cb91a32713bb74198128
YusufYuso <yusuf.elsaed@gmail.com>
2023-07-31 18:27:38 -0700

    Adding File: 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,16 +12,13 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j;
 
-	while (s[i])
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != s[i]; j++)
 		{
 			if (accept[j] == '\0')
 				return (i);
 		}
-
-		i++;
 	}
-
 	return (i);
 }
