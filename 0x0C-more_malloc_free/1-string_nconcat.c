34ea8d695cd5edd1dccccc1ae5e39231e2009b61
YusufYuso <yusuf.elsaed@gmail.com>
2023-08-11 02:57:45 +0300

    Adding C source, ASCII text: 1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * string_nconcat - Concatenates two strings using at
+ *                  most an inputted number of bytes.
+ * @s1: The first string.
+ * @s2: The second string.
+ * @n: The maximum number of bytes of s2 to concatenate to s1.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - a pointer to the concatenated space in memory.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ptr;
+	unsigned int len, i;
+
+	len = n;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i]; i++)
+		len++;
+
+	ptr = malloc(sizeof(char) * (len + 1));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	len = 0;
+	i = 0;
+
+	while (s1[i])
+		ptr[len++] = s1[i++];
+
+	for (i = 0; s2[i] && i < n; i++)
+		ptr[len++] = s2[i];
+
+	ptr[len] = '\0';
+
+	return (ptr);
+}
