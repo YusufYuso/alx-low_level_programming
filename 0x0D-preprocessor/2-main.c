e44393eb14fb23d1ee34af97ba7c962eb46e7dfa
YusufYuso <yusuf.elsaed@gmail.com>
2023-08-15 02:01:12 +0300

    Adding C source, ASCII text: 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * to print name of the current source file.
+ *
+ * Return: Always (0) on success.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
