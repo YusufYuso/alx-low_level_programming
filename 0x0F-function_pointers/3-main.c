a507f34d743e4b020df5645b92e6f7a86f49eb89
YusufYuso <yusuf.elsaed@gmail.com>
2023-08-17 00:38:36 +0300

    Adding C source, ASCII text: 3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+
+/**
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	int n1, n2;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	n1 = atoi(argv[1]);
+	op = argv[2];
+	n2 = atoi(argv[3]);
+
+	if (op[1] != '\0' || get_op_func(op) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (n2 == 0 && (*op == '/' || *op == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(op)(n1, n2));
+	return (0);
+}
