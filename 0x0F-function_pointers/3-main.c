dd85deeb855c565d52decdf2879c4e2e7212db05
YusufYuso <yusuf.elsaed@gmail.com>
2023-08-17 01:26:15 +0300

    modify

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,8 +9,9 @@
  */
 int main(int argc, char **argv)
 {
-	int n1, n2;
+	int n1, n2, value;
 	char *op;
+	int (*fun)(int, int);
 
 	if (argc != 4)
 	{
@@ -20,8 +21,9 @@ int main(int argc, char **argv)
 	n1 = atoi(argv[1]);
 	op = argv[2];
 	n2 = atoi(argv[3]);
+	fun = get_op_func(op);
 
-	if (op[1] != '\0' || get_op_func(op) == NULL)
+	if (!fun)
 	{
 		printf("Error\n");
 		exit(99);
@@ -32,6 +34,7 @@ int main(int argc, char **argv)
 		exit(100);
 	}
 
-	printf("%d\n", get_op_func(op)(n1, n2));
+	value = f(n1, n2);
+	printf("%d\n", value);
 	return (0);
 }
