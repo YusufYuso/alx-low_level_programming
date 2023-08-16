d4f0c7467a159c23ca91940b47d273112c496d17
YusufYuso <yusuf.elsaed@gmail.com>
2023-08-17 02:03:45 +0300

    another

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,8 +20,12 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (*(ops[i].op) != *s && ops[i].op != NULL)
+	while (i < 5)
+	{
+		if (s && s[0] == ops[i].op[0] && s[1] == '\0')
+			return (ops[i].f);
 		i++;
+	}
 
-	return (ops[i].f);
+	return (NULL);
 }
