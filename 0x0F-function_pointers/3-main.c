002a11b498ddb4046a888ab3ff7149d00769f39b
YusufYuso <yusuf.elsaed@gmail.com>
2023-08-17 01:55:24 +0300

    try

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,9 +7,10 @@
  *
  * Return: Always 0.
  */
-int main(int __attribute__((__unused__)) argc, char **argv)
+int main(int argc, char **argv)
 {
-	int (*func)(int, int), n1, n2;
+	int n1, n2;
+	char *op;
 
 	if (argc != 4)
 	{
@@ -17,20 +18,20 @@ int main(int __attribute__((__unused__)) argc, char **argv)
 		exit(98);
 	}
 	n1 = atoi(argv[1]);
-	func = get_op_func(argv[2]);
+	op = argv[2];
 	n2 = atoi(argv[3]);
 
-	if (!func)
+	if (op[1] != '\0' || get_op_func(op) == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((n2 == 0 && (argv[2][0]) == '/') || (n2 == 0 && (argv[2][0]) == '%'))
+	if ((n2 == 0 && *op == '/') || (n2 == 0 && *op == '%'))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", func(n1, n2));
+	printf("%d\n", get_op_func(op)(n1, n2));
 	return (0);
 }
