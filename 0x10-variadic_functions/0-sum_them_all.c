b9a82238c9b5dbdcbd5838a2e95ddd1157aea428
YusufYuso <yusuf.elsaed@gmail.com>
2023-08-17 23:02:39 +0300

    Adding C source, ASCII text file: ../0x10-variadic_functions/0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,17 @@
+#include "variadic_functions.h"
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i,sum;
+
+	va_start (args,n);
+
+	sum=0;
+	for (i=0; i<n;i++)
+	{
+		sum+= va_arg(args,int);
+	}
+	va_end(args);
+	return(sum);
+}
