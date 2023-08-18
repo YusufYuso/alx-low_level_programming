d69226b369e212a3b4c6cfbf655a18f863f4d04a
YusufYuso <yusuf.elsaed@gmail.com>
2023-08-18 10:22:34 +0300

    Updating C source, ASCII text file: 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,6 +1,17 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 
+/**
+ * print_all - Prints anything, followed by a new line.
+ * @format: A string of characters representing the argument types.
+ * @...: A variable number of arguments to be printed.
+ *
+ * Description: Any argument not of type char, int, float,
+ *              or char * is ignored.
+ *              If a string argument is NULL, (nil) is printed instead.
+ *
+ * Return: it returns nothing
+ */
 void print_all(const char *const format, ...)
 {
 	va_list args;
