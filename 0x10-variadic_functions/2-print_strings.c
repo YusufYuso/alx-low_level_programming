b9f8e868542ab6f95fad2ea506b33d665ed5379d
YusufYuso <yusuf.elsaed@gmail.com>
2023-08-18 05:44:15 +0300

    Updating C source, ASCII text file: 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,6 +1,31 @@
 #include "variadic_functions.h"
+#include <stdio.h>
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	
+	va_list args;
+	unsigned int i;
+	char *s;
+
+	if (!n && separator == NULL)
+	{
+		printf("\n");
+		return;
+	}
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(args, char *);
+		if (!s)
+		{
+			printf("(nil)");
+		}
+		printf("%s", s);
+		if (i != (n - 1))
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
 }
