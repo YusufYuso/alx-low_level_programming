b8678eeb8b761ca775046c3b3813379db2a93093
YusufYuso <yusuf.elsaed@gmail.com>
2023-08-25 05:29:35 +0300

    Updating C source, ASCII text file: 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,1 +1,11 @@
-#include "lists.h"
+#include <stdio.h>
+
+void before_main() __attribute__((constructor));
+/**
+ * before_main - Prints a string before the
+ *        main function is executed.
+ */
+void before_main()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
