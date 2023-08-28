f1e20d0474dcd37234cc5b2bea017007df7c32f1
YusufYuso <yusuf.elsaed@gmail.com>
2023-08-28 09:56:07 +0300

    Updating C source, ASCII text file: 0x12-singly_linked_lists/lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,7 +5,7 @@
 
 /**
  * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
+ * @: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
  *
@@ -16,7 +16,8 @@ typedef struct list_s
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-} list_t;
+} 
+list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
