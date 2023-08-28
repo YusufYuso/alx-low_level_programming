9a1f2eef3cfc9e84185bd5726119dcafe6676773
YusufYuso <yusuf.elsaed@gmail.com>
2023-08-28 13:40:09 +0300

    Updating C source, ASCII text file: 0x12-singly_linked_lists/lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,7 +5,7 @@
 
 /**
  * struct list_s - singly linked list
- * @: string - (malloc'ed string)
+ * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
  *
