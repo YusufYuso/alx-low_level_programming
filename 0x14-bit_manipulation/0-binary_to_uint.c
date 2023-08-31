eb7c01af079c3fe0ed1e4cc9440ee4ca53e45895
YusufYuso <yusuf.elsaed@gmail.com>
2023-09-01 00:01:15 +0300

    Updating C source, ASCII text file: 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,21 +9,15 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int n = 0, m = 1;
-	int l;
+	unsigned int n = 0;
 
-	if (b == '\0')
+	if (!b)
 		return (0);
-	for (l = 0; b[l];)
+	while (*b)
 	{
-		l++;
-	}
-	for (l -= 1; l >= 0; l--)
-	{
-		if (b[l] != '0' && b[l] != 1)
+		if (*b != '0' && *b != '1')
 			return (0);
-		n += (b[l] - '0') * m;
-		m *= 2;
+		n = n * 2 + (*b++ - '0');
 	}
 	return (n);
 }
