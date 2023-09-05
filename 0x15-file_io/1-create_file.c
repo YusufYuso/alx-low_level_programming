4c03167935f98cb4f14f440de0a1a55d722f7e2d
YusufYuso <yusuf.elsaed@gmail.com>
2023-09-05 03:53:26 +0300

    Updating C source, ASCII text file: 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,9 +23,12 @@ int create_file(const char *filename, char *text_content)
 	f = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (f == -1)
 		return (-1);
+
 	w = write(f, text_content, l);
+	if (f == -1)
+		return (-1);
 
 	close(f);
 
-	return (w == l ? 1 : -1);
+	return (1);
 }
