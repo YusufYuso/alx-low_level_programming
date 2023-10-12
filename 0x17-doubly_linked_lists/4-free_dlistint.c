6020f32fe002499b49c2e8b8ee09ef4cd438876f
YusufYuso <yusuf.elsaed@gmail.com>
2023-10-12 20:33:09 +0300

    Updating C source, ASCII text file: 4-free_dlistint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,1 +1,17 @@
 #include "lists.h"
+
+/**
+ * free_dlistint - Frees a doubly linked list
+ * @head: A pointer to the head of the doubly linked list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *node;
+
+	while (head)
+	{
+		node = head;
+		head = head->next;
+		free(node);
+	}
+}
