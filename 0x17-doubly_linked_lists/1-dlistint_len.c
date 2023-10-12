395f23b7c1c5e03fafa8fb7eda9db1a10adbe8e4
YusufYuso <yusuf.elsaed@gmail.com>
2023-10-12 17:03:05 +0300

    Updating C source, ASCII text file: 1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,1 +1,19 @@
 #include "lists.h"
+
+/**
+ * dlistint_len - Count the number of elements in a doubly linked list
+ * @h: A pointer to the head of the doubly linked list
+ * Return: The number of elements in the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int len = 0;
+
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+
+	return (len);
+}
