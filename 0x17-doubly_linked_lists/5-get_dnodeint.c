a496e77c58da5a89f8de46cf0feffc5cd39d958c
YusufYuso <yusuf.elsaed@gmail.com>
2023-10-12 20:46:26 +0300

    Updating C source, ASCII text file: 5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * get_dnodeint_at_index - Returns the node at a given index in a doubly linked list
+ * @head: A pointer to the head of the doubly linked list
+ * @index: The index of the node to retrieve
+ * Return: A pointer to the node at the specified index, or NULL if not found
+ */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int idx = 0;
