b037710ceb18b6a737cf31dcea886c151ad0cba6
YusufYuso <yusuf.elsaed@gmail.com>
2023-10-12 18:03:38 +0300

    Updating C source, ASCII text file: 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,1 +1,19 @@
 #include "lists.h"
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new, *current;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	current = *head;
+	new->n = n;
+	current->prev = new;
+	new->next = current;
+	new->prev = NULL;
+	current = new;
+
+	return (current);
+}
