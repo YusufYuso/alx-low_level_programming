0975acee213e44ac31fb8b657d0443dda199cfa4
YusufYuso <yusuf.elsaed@gmail.com>
2023-10-12 16:48:34 +0300

    Updating C source, ASCII text file: 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,2 +1,20 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - Print all elements of a doubly linked list
+ * @h: A pointer to the head of the doubly linked list
+ * Return: The number of nodes in the list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
