db857483ef32efa1e180b8f1327f4e9b33b4c9a5
YusufYuso <yusuf.elsaed@gmail.com>
2023-10-12 20:52:14 +0300

    Updating C source, ASCII text file: 6-sum_dlistint.c

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,1 +1,18 @@
 #include "lists.h"
+
+/**
+ * sum_dlistint - Computes the sum of all the integers in a doubly linked list
+ * @head: A pointer to the head of the doubly linked list
+ * Return: The sum of all integers in the list
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
