c11d52ed1c213064a68739616ee683a3572d6585
YusufYuso <yusuf.elsaed@gmail.com>
2023-10-12 18:08:58 +0300

    Updating C source, ASCII text file: 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * add_dnodeint - Add a new node at the beginning of a doubly linked list
+ * @head: A pointer to a pointer to the head of the doubly linked list
+ * @n: The integer value to store in the new node
+ * Return: A pointer to the newly added node, or NULL if it fails
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
