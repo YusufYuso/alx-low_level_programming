c0aaf12899ec9d0b6a5ba1f5a88b1d1619a5e326
YusufYuso <yusuf.elsaed@gmail.com>
2023-11-02 18:22:38 +0200

    Updating C source, ASCII text file: 3-hash_table_set.c

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,57 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Adds an element to the hash table.
+ * @ht: The hash table to which the element is added.
+ * @key: The key of the element.
+ * @value: The value associated with the key.
+ *
+ * Return: 1 if successful, 0 if an error occurs.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_node, *current;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || value == NULL || strlen(key) == 0)
+		return (0);
+
+	index = key_index((unsigned char *)key, ht->size);
+	current = ht->array[index];
+
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = strdup(value);
+			if (current->value == NULL)
+				return (0);
+			return (1);
+		}
+		current = current->next;
+	}
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
+
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
